c77bbeede5ef33fa8e1439621d03727aaa6bfa5b
Max <max.hertenstein@gmx.de>
2025-09-11 08:43:47 +0200

    update

---
+++ projekte/Snake/src/test/java/org/example/V2Test.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class V2Test {
+
+    @org.junit.jupiter.api.Test
+    void plus() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void times() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void x() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void y() {
+    }
+}
